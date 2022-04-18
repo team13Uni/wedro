18b0b330986596b77e8eff37716c7b2f20a61184
Martin Foldyna <martin.foldyna@me.com>
2022-04-18 21:29:11 +0200

    Add HttpException

---
+++ server/src/exceptions/HttpException.ts
@@ -0,0 +1,14 @@
+export class HttpException extends Error {
+  status: number;
+  message: string;
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
+
+export interface IError extends Error {
+  status: number;
+  statusCode?: number;
+}
